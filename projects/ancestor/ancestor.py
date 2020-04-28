c56d5e00a61c2467cf4e8c073ef28c387431a623
malarc01 <email@example.com>
2020-04-28 19:30:39 -0400

    ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,38 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # building a graph
+    graph = Graph()
+
+    for pair in ancestors:
+        parent = pair[0]
+        child = pair[1]
+        graph.add_vertex(parent)
+        graph.add_vertex(child)
+        graph.add_edge(child, parent)
+
+    # Breath For Search
+    queue = Queue()
+    queue.enqueue([starting_node])
+
+    longest_path_length = 1
+    earliest_ancestor = -1
+
+    while queue.size() > 0:
+        path = queue.dequeue()
+        current_node = path[-1]
+
+        if len(path) >= longest_path_length:
+            if current_node < earliest_ancestor:
+                longest_path_length = len(path)
+                earliest_ancestor = current_node
+
+        if len(path) > longest_path_length:
+            longest_path_length = len(path)
+            earliest_ancestor = current_node
+
+        neighbors = graph.vertices[current_node]
+        for ancestor in neighbors:
+            path_copy = list(path)
+            path_copy.append(ancestor)
+            queue.enqueue(path_copy)
+    return earliest_ancestor
